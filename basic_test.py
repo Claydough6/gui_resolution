aa20811064e70981c5f4e8a9c8d800fedebe62bd
Claydough6 <claybell01@gmail.com>
2021-04-24 19:25:30 -0400

    Update basic_test.py
    Added functionality (if you could call it that) to the test gui file.

--- basic_test.py
+++ basic_test.py
@@ -1,6 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 
+# create the main window
 root = Tk()
 
+# make a frame for the widget to live in
+frame = ttk.Frame(root)
+frame['padding'] = (5, 10)  # vertical, horizontal padding
+frame['borderwidth'] = 2
+frame['relief'] = 'ridge'  # just styling
+frame.grid()
+
+# create a button for example (lives in the frame)
+button = ttk.Button(frame, text="push me")
+button.grid()
+
+# update the theme of the test app
+s = ttk.Style()
+s.theme_use('xpnative')
+
+# start up the frame
 root.mainloop()
