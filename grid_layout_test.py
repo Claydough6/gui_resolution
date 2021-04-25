8cd3ad07fc90d83df65c49a55ca7a807f8daaab7
Claydough6 <claybell01@gmail.com>
2021-04-25 11:18:01 -0400

    Update grid_layout_test.py
    Added the feather image and added padding for the boxes to space out better.

--- grid_layout_test.py
+++ grid_layout_test.py
@@ -7,7 +7,6 @@ root = Tk()
 # create the main frame of the program and the sub frame
 content = ttk.Frame(root)
 subframe = ttk.Frame(content, borderwidth=5, relief='sunken')
-topframe = ttk.Frame(content, borderwidth=5, relief='raised', width=200, height=100)
 
 # make the checks to go in the subframe
 c1 = ttk.Checkbutton(subframe, text="One")
@@ -19,18 +18,23 @@ ok = ttk.Button(subframe, text="Click Me!")
 label = ttk.Label(content, text="Enter some text:")
 box = ttk.Entry(content)
 
+# put the image in another box
+feather = PhotoImage(file="feather.png")
+pic = ttk.Label(content)
+pic['image'] = feather
+
 # lay everything out in the app
 content.grid(column=0, row=0)
-#topframe.grid(column=0, row=0, columnspan=5, rowspan=2)
-subframe.grid(column=0, row=2, columnspan=5)
+subframe.grid(column=0, row=2, columnspan=5 ,pady=5)
 
-c1.grid(column=0, row=2)
-c2.grid(column=1, row=2)
-c3.grid(column=2, row=2)
-ok.grid(column=3, row=2, columnspan=2)
+c1.grid(column=0, row=2, padx=5)
+c2.grid(column=1, row=2, padx=5)
+c3.grid(column=2, row=2, padx=5)
+ok.grid(column=3, row=2, columnspan=2, padx=5)
 
-label.grid(column=3, row=0, columnspan=2)
-box.grid(column=3, row=1, columnspan=2)
+label.grid(column=3, row=0, columnspan=2, padx=15)  # pad edges nicely
+box.grid(column=3, row=1, columnspan=2, padx=15)
+pic.grid(column=0, row=0, columnspan=3, rowspan=2)
 
 # run the app
 root.mainloop()
