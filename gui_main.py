ed8b8e4584d7988ffd84f4f132fff6d20b7fd69f
Claydough6 <claybell01@gmail.com>
2021-04-27 14:51:48 -0400

    Update gui_main.py
    Added some buttons and frame scaling.

--- gui_main.py
+++ gui_main.py
@@ -2,9 +2,11 @@ from tkinter import *
 from tkinter import ttk
 from ttkthemes import ThemedTk
 
+__version__ = 0.1
+
 # create the root window
-root = ThemedTk(theme="breeze")
-root.title("Graphical Resolution")
+root = Tk()
+root.title("Graphical Resolution " + str(__version__))
 root.option_add('*tearOff', FALSE)  # so the menu doesn't tear
 
 # used to display the help menu
@@ -33,7 +35,7 @@ resolution = ttk.Labelframe(root, text="Resolution:", relief="raised")
 resolution['width'] = 500
 resolution['height'] = 400
 
-statements = ttk.Labelframe(root, text="Statements")
+statements = ttk.Labelframe(root, text="Statements:")
 statements['width'] = 100
 statements['height'] = 250
 
@@ -41,10 +43,25 @@ tools = ttk.Frame(root)
 tools['width'] = 100
 tools['height'] = 150
 
+# create the buttons for the tools
+newPremise = ttk.Button(tools, text="New Premise")
+editPremise = ttk.Button(tools, text="Edit Premise")
+editConclusion = ttk.Button(tools, text="Edit Conclusion")
+
 # grid everything into the app
-resolution.grid(row=0, column=1, rowspan=3, padx=5, pady=5)
-statements.grid(row=1, column=0, padx=5)
-tools.grid(row=2, column=0, padx=5)
+resolution.grid(row=0, column=1, rowspan=3, padx=5, pady=5, sticky=(N,E,S,W))
+statements.grid(row=0, column=0, padx=5, pady=5, sticky=(N,E,S,W))
+tools.grid(row=1, column=0, padx=5)
+
+newPremise.grid(pady=2)
+editPremise.grid(pady=2)
+editConclusion.grid(pady=2)
+
+# specify how columns and row expand
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=3)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
 
 # start up the app
 root.mainloop()
