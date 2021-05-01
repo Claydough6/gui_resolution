e56f5127e68ce9114f210fa203075c8c931ac2ec
Claydough6 <claybell01@gmail.com>
2021-05-01 17:23:38 -0400

    Clause Frames and Parent Lines
    Added a class for the clause frames. Also added the line drawing functionality and ability to move frames with lines. Created a new class for the statement frame as well.

--- gui_main.py
+++ gui_main.py
@@ -4,8 +4,9 @@ from ttkthemes import ThemedTk
 
 from edit_listbox import EditListbox
 from resolution_canvas import ResolutionCanvas
+from statement_frame import StatementFrame
 
-__version__ = 0.2
+__version__ = 0.3
 
 # create the root window
 root = ThemedTk()
@@ -37,48 +38,17 @@ root.configure(menu=menubar)
 
 # create the main frames used
 resolution = ttk.Labelframe(root, text="Resolution:")
-leftframe = ttk.Frame(root)
-
-statements = ttk.Labelframe(leftframe, text="Statements:")
-tools = ttk.Frame(leftframe)
-
-# create the list of premises
-plist = EditListbox(statements, selectmode="browse")
-plist.colorize()
-
-# create the conclusion list
-clist = EditListbox(statements, selectmode="browse", height=1)
-clist.insert(0, "Conclusion")
-clist.color = "IndianRed1"
-clist.colorize()
-
-# create the buttons for the tools
-newPremise = ttk.Button(tools, text="New Premise", command=plist.add_premise)
-deletePremise = ttk.Button(tools, text="Delete Premise", command=plist.remove_premise)
+leftframe = StatementFrame(root)
 
 # create the canvas to do the resolution on
 canvas = ResolutionCanvas(resolution)
 canvas.configure(bg='white')
 
-# create the scrollbar
-sbar = ttk.Scrollbar(statements, orient=VERTICAL, command=plist.yview)
-plist.configure(yscrollcommand=sbar.set)
-
 # grid everything into the app
 resolution.grid(row=0, column=1, rowspan=3, padx=5, pady=5, sticky=(N,E,S,W))
 leftframe.grid(row=0, column=0, sticky=(N,E,S,W))
 
-statements.grid(row=0, column=0, padx=5, pady=5, sticky=(N,E,S,W))
-tools.grid(row=1, column=0, padx=5)
-
-newPremise.grid(pady=2)
-deletePremise.grid(pady=2)
-
 canvas.grid(sticky=(N,E,S,W))
-
-plist.grid(row=0, column=0, sticky=(N,E,S,W))
-clist.grid(row=1, column=0, sticky=(E,W,S))
-sbar.grid(row=0, column=1, sticky=(N,S,E))
 
 # setup a sizegrip item at the bottom right corner
 ttk.Sizegrip().grid(column=1, row=1, sticky=(S,E))
@@ -92,11 +62,8 @@ resolution.grid_columnconfigure(0, weight=1)
 resolution.grid_rowconfigure(0, weight=1)
 
 leftframe.grid_columnconfigure(0, weight=1)
-leftframe.grid_rowconfigure(0, weight=2)
+leftframe.grid_rowconfigure(0, weight=5)
 leftframe.grid_rowconfigure(1, weight=1)
-
-statements.grid_columnconfigure(0, weight=1)
-statements.grid_rowconfigure(0, weight=1)
 
 # start up the app
 root.mainloop()
