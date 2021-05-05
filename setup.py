c677b3d5805145b98da91208ca4b380fbeb12017
Claydough6 <33795853+Claydough6@users.noreply.github.com>
2021-05-04 23:25:03 -0400

    Create setup.py
    Used to install package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="resolution_gui",
+      version="1.0",
+      description="A resolution GUI app for prop logic",
+      url="https://github.com/Claydough6/gui_resolution",
+      author="Clay Bell and Kevin Khaghani",
+      author_email="claybell01@gmail.com",
+      license="MIT",
+      packages=["resolution_gui"],
+      zip_safe=False)
